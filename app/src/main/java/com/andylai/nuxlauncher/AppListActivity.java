b654d2ab4a64fb16c91725b7c3838e14e74ae2c9
Andy Lai <badboyandy13@gmail.com>
2017-10-31 01:34:51 +0800

    Add function queryAllApps

--- app/src/main/java/com/andylai/nuxlauncher/AppListActivity.java
+++ app/src/main/java/com/andylai/nuxlauncher/AppListActivity.java
@@ -1,6 +1,7 @@
 package com.andylai.nuxlauncher;
 
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
 import android.os.Bundle;
@@ -16,17 +17,21 @@ import java.util.List;
 public class AppListActivity extends AppCompatActivity {
 
     String[] app_name = {"app1", "app2"};
-
+    PackageManager mPackageManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_list);
+
+        mPackageManager = getPackageManager();
+
+
         ListView AppList = (ListView) findViewById(R.id.app_list_view);
         ListAdapter adapter = new ArrayAdapter<>(this ,
-                android.R.layout.simple_list_item_1 ,app_name);
+                android.R.layout.simple_list_item_1 ,queryAllApps());
         AppList.setAdapter(adapter);
 
-        queryHomeApp();
+
 
 //        adapter.addItemList(queryHomeApp());
 //        lvHome.setAdapter(adapter);
@@ -51,21 +56,50 @@ public class AppListActivity extends AppCompatActivity {
     private List<HomeInfo> queryHomeApp() {
         Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
         mainIntent.addCategory(Intent.CATEGORY_HOME);
-        PackageManager pm = getPackageManager();
-        List<ResolveInfo> rList = pm.queryIntentActivities(mainIntent, 0);
+
+        List<ResolveInfo> rList = mPackageManager.
+                queryIntentActivities(mainIntent, 0);
         List<HomeInfo> homeList = new ArrayList<HomeInfo>();
 
         for(ResolveInfo resolveInfo : rList) {
             Log.d("Andy", "resolveInfo = " + resolveInfo);
-            homeList.add(new HomeInfo(resolveInfo, pm));
+            homeList.add(new HomeInfo(resolveInfo, mPackageManager));
 
         }
         return homeList;
     }
 
+    private List<ApplicationInfo> checkForLaunchIntent(List<ApplicationInfo> list) {
+        ArrayList<ApplicationInfo> applist = new ArrayList<ApplicationInfo>();
+        for (ApplicationInfo info : list) {
+            try {
+                if (null != mPackageManager.getLaunchIntentForPackage(info.packageName)) {
+                    applist.add(info);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        return applist;
+    }
 
 
+    private ArrayList<String> queryAllApps() {
 
+        ArrayList<String> AllApps = new ArrayList<>();
+        String pkgAndClassName = null;
 
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        List<ResolveInfo> resolveInfoList = mPackageManager.
+                queryIntentActivities(intent, 0);
+        for (ResolveInfo resolveInfo: resolveInfoList) {
+            Log.d("Andy", "resolveInfo = " + resolveInfo.loadLabel(mPackageManager));
+            Log.d("Andy", "toString = " + resolveInfo.toString());
+            AllApps.add((String)resolveInfo.loadLabel(mPackageManager));
+        }
+        return AllApps;
+    }
 }
 
